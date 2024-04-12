b617b7368f11515c89ebdce862309f7af2c5a911
Josh Logan <joshlogan103@gmail.com>
2024-04-12 13:17:00 -0400

    Updated get all players fetch request on home page to only show 4 players and key fields

--- home/home.js
+++ home/home.js
@@ -28,20 +28,21 @@ function getAllPlayers() {
     .then(res => res.json())
     .then(data => {
       if (!data.length > 0) return
-      data.forEach(player => {
+      for (let i = 0; i < 4; i++){
+        let player = data[i]
         const newPlayerUl = document.createElement('ul')
         newPlayerUl.classList.add('player-ul')
         Object.keys(player).forEach(key => {
-          const newPlayerFieldLi = document.createElement('li')
-          newPlayerFieldLi.classList.add('player-field-li')
-          if (key === 'tournamentsPlayed') {
-            player[key].join(', ')
+          const fields = ['fullName', 'age', 'nationality', 'worldRanking', 'lifetimeEarnings']
+          if (fields.includes(key)) {
+            const newPlayerFieldLi = document.createElement('li')
+            newPlayerFieldLi.classList.add('player-field-li')
+            newPlayerFieldLi.textContent = `${key}: ${player[key]}`
+            newPlayerUl.appendChild(newPlayerFieldLi)
           }
-          newPlayerFieldLi.textContent = `${key}: ${player[key]}`
-          newPlayerUl.appendChild(newPlayerFieldLi)
         })
         playersDiv.appendChild(newPlayerUl)
-      })
+      }
     })
 }
 
