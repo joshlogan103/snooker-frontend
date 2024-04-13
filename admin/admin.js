eb18129c77ada7e5860a892fef0ad1ea9dbff8d7
Josh Logan <joshlogan103@gmail.com>
2024-04-13 14:44:51 -0400

    Add delete function to admin portal

--- admin/admin.js
+++ admin/admin.js
@@ -10,20 +10,53 @@ const lastNamePlayerInput = document.getElementById('last-name-player')
 const agePlayerInput = document.getElementById('age-player')
 const nationalityPlayerInput = document.getElementById('nationality-player')
 const worldRankingPlayerInput = document.getElementById('world-ranking-player')
-// Add update element caches here
 
 // Create tournament form elements
 
 const createTournamentSubmitButton = document.getElementById('create-tournament-button') 
-const nameTournament = document.getElementById('name-tournament')
-const prizeMoneyTournament = document.getElementById('prize-money-tournament')
-const startDateTournament = document.getElementById('start-date-tournament')
-const endDateTournament = document.getElementById('end-date-tournament')
-const countryTournament = document.getElementById('country-tournament')
-const cityTournament = document.getElementById('city-tournament')
-const venueNameTournament = document.getElementById('venue-name-tournament')
-const positionsTournamanet = document.getElementById('leaderboard-positions-tournament')
-const prizeBreakdownTournament = document.getElementById('prize-breakdown-tournament')
+const nameTournamentInput = document.getElementById('name-tournament')
+const prizeMoneyTournamentInput = document.getElementById('prize-money-tournament')
+const startDateTournamentInput = document.getElementById('start-date-tournament')
+const endDateTournamentInput = document.getElementById('end-date-tournament')
+const countryTournamentInput = document.getElementById('country-tournament')
+const cityTournamentInput = document.getElementById('city-tournament')
+const venueNameTournamentInput = document.getElementById('venue-name-tournament')
+const positionsTournamentInput = document.getElementById('leaderboard-positions-tournament')
+const prizeBreakdownTournamentInput = document.getElementById('prize-breakdown-tournament')
+
+// Update player form elements
+
+const updatePlayerSubmitButton = document.getElementById('update-player-button')
+const idPlayerUpdateInput = document.getElementById('id-player-update')
+const firstNamePlayerUpdateInput = document.getElementById('first-name-player-update')
+const lastNamePlayerUpdateInput = document.getElementById('last-name-player-update')
+const agePlayerUpdateInput = document.getElementById('age-player-update')
+const nationalityPlayerUpdateInput = document.getElementById('nationality-player-update')
+const worldRankingPlayerUpdateInput = document.getElementById('world-ranking-player-update')
+
+// Update tournament form elements
+
+const updateTournamentSubmitButton = document.getElementById('update-tournament-button') 
+const idTournamentUpdateInput = document.getElementById('id-tournament-update')
+const nameTournamentUpdateInput = document.getElementById('name-tournament-update')
+const prizeMoneyTournamentUpdateInput = document.getElementById('prize-money-tournament-update')
+const startDateTournamentUpdateInput = document.getElementById('start-date-tournament-update')
+const endDateTournamentUpdateInput = document.getElementById('end-date-tournament-update')
+const countryTournamentUpdateInput = document.getElementById('country-tournament-update')
+const cityTournamentUpdateInput = document.getElementById('city-tournament-update')
+const venueNameTournamentUpdateInput = document.getElementById('venue-name-tournament-update')
+const positionsTournamentUpdateInput = document.getElementById('leaderboard-positions-tournament-update')
+const prizeBreakdownTournamentUpdateInput = document.getElementById('prize-breakdown-tournament-update')
+
+// Delete player form elements
+
+const deletePlayerSubmitButton = document.getElementById('delete-player-button')
+const idPlayerDeleteInput = document.getElementById('id-player-delete')
+
+// Delete tournament form elements
+
+const deleteTournamentSubmitButton = document.getElementById('delete-tournament-button') 
+const idTournamentDeleteInput = document.getElementById('id-tournament-delete')
 
 // Protect this page to only logged in admins
 
@@ -52,16 +85,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 createPlayerSubmitButton.addEventListener('click', createPlayer)
 createTournamentSubmitButton.addEventListener('click', createTournament)
+updatePlayerSubmitButton.addEventListener('click', updatePlayer)
+updateTournamentSubmitButton.addEventListener('click', updateTournament)
+deletePlayerSubmitButton.addEventListener('click', deletePlayer)
+deleteTournamentSubmitButton.addEventListener('click', deleteTournament)
 
 // Retrieve JWT token from local storage
 
-function getAccessToken() {
-  return localStorage.getItem('jwtToken')
-}
+// function getAccessToken() {
+//   return localStorage.getItem('jwtToken')
+// }
 
 // Functions
 
-// Create a player
+// Creates a new player in the database based on the html form
 
 function createPlayer(e) {
   e.preventDefault()
@@ -88,19 +125,21 @@ function createPlayer(e) {
   worldRankingPlayerInput.value = ''
 }
 
+// Creates a new tournament in the database based on the html form
+
 function createTournament(e) {
   e.preventDefault()
-  const positions = positionsTournamanet.value.split(', ')
-  const prizeBreakdown = prizeBreakdownTournament.value.split(', ')
+  const positions = positionsTournamentInput.value.split(', ')
+  const prizeBreakdown = prizeBreakdownTournamentInput.value.split(', ')
   const newTournament = {
-    name: nameTournament.value,
-    prizeMoney: prizeMoneyTournament.value,
-    startDate: startDateTournament.value,
-    endDate: endDateTournament.value,
+    name: nameTournamentInput.value,
+    prizeMoney: prizeMoneyTournamentInput.value,
+    startDate: startDateTournamentInput.value,
+    endDate: endDateTournamentInput.value,
     location: {
-      country: countryTournament.value,
-      city: cityTournament.value,
-      venueName: venueNameTournament.value
+      country: countryTournamentInput.value,
+      city: cityTournamentInput.value,
+      venueName: venueNameTournamentInput.value
     },
     leaderboard: {
       positions: positions,
@@ -116,13 +155,126 @@ function createTournament(e) {
     .then(data => {
       console.log(data)
     })
-  nameTournament.value = ''
-  prizeMoneyTournament.value = ''
-  startDateTournament.value = ''
-  endDateTournament.value = ''
-  countryTournament.value = ''
-  cityTournament.value = ''
-  venueNameTournament.value = ''
-  positionsTournamanet.value = ''
-  prizeBreakdownTournament.value = ''
+  nameTournamentInput.value = ''
+  prizeMoneyTournamentInput.value = ''
+  startDateTournamentInput.value = ''
+  endDateTournamentInput.value = ''
+  countryTournamentInput.value = ''
+  cityTournamentInput.value = ''
+  venueNameTournamentInput.value = ''
+  positionsTournamentInput.value = ''
+  prizeBreakdownTournamentInput.value = ''
 }
+
+// Updates an existing player in the database based on the html form
+
+function updatePlayer(e) {
+  e.preventDefault()
+  const playerId = idPlayerUpdateInput.value
+  const updatedPlayer = {}
+  if (firstNamePlayerUpdateInput.value) {updatedPlayer.firstName = firstNamePlayerUpdateInput.value}
+  if (lastNamePlayerUpdateInput.value) {updatedPlayer.lastName = lastNamePlayerUpdateInput.value}
+  if (agePlayerUpdateInput.value) {updatedPlayer.age = agePlayerUpdateInput.value}
+  if (nationalityPlayerUpdateInput.value) {updatedPlayer.nationality = nationalityPlayerUpdateInput.value}
+  if (worldRankingPlayerUpdateInput.value) {updatedPlayer.worldRanking = worldRankingPlayerUpdateInput.value}
+
+  console.log(updatedPlayer)
+  fetch(`${databaseURL}/players/${playerId}`,{
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json',
+    'Authorization': `Bearer ${getAccessToken()}`},
+    body: JSON.stringify(updatedPlayer) })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  idPlayerUpdateInput.value = ''
+  firstNamePlayerUpdateInput.value = ''
+  lastNamePlayerUpdateInput.value = ''
+  agePlayerUpdateInput.value = ''
+  nationalityPlayerUpdateInput.value = ''
+  worldRankingPlayerUpdateInput.value = ''
+}
+
+// Creates a new tournament in the database based on the html form
+
+function updateTournament(e) {
+  e.preventDefault()
+  const tournamentId = idTournamentUpdateInput.value
+  const positions = positionsTournamentUpdateInput.value.split(', ')
+  const prizeBreakdown = prizeBreakdownTournamentUpdateInput.value.split(', ').map(prize => parseFloat(prize))
+  const updatedTournament = {}
+  const location = {}
+  const leaderboard = {}
+
+  if (nameTournamentUpdateInput.value) {updatedTournament.name = nameTournamentUpdateInput.value}
+  if (prizeMoneyTournamentUpdateInput.value) {updatedTournament.prizeMoney = parseFloat(prizeMoneyTournamentUpdateInput.value)}
+  if (startDateTournamentUpdateInput.value) {updatedTournament.startDate = new Date(startDateTournamentUpdateInput.value)}
+  if (endDateTournamentUpdateInput.value) {updatedTournament.endDate = new Date(endDateTournamentUpdateInput.value)}
+  if (countryTournamentUpdateInput.value) {location.country = countryTournamentUpdateInput.value}
+  if (cityTournamentUpdateInput.value) {location.city = cityTournamentUpdateInput.value}
+  if (venueNameTournamentUpdateInput.value) {location.venueName = venueNameTournamentUpdateInput.value}
+  if (positionsTournamentUpdateInput.value) {leaderboard.positions = positions}
+  if (prizeBreakdownTournamentUpdateInput.value) {leaderboard.prizeMoneyBreakdown = prizeBreakdown}
+
+  if (Object.keys(location).length !== 0) {updatedTournament.location = location}
+  if (Object.keys(leaderboard).length !== 0) {updatedTournament.leaderboard = leaderboard}
+
+  console.log(location)
+  console.log(leaderboard)
+  console.log(updatedTournament)
+
+  fetch(`${databaseURL}/tournaments/${tournamentId}`,{
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json',
+    'Authorization': `Bearer ${getAccessToken()}`},
+    body: JSON.stringify(updatedTournament) })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  idTournamentUpdateInput.value = ''
+  nameTournamentUpdateInput.value = ''
+  prizeMoneyTournamentUpdateInput.value = ''
+  startDateTournamentUpdateInput.value = ''
+  endDateTournamentUpdateInput.value = ''
+  countryTournamentUpdateInput.value = ''
+  cityTournamentUpdateInput.value = ''
+  venueNameTournamentUpdateInput.value = ''
+  positionsTournamentUpdateInput.value = ''
+  prizeBreakdownTournamentUpdateInput.value = ''
+}
+
+// Delete a player in the databse by entering the player ID
+
+function deletePlayer(e) {
+  e.preventDefault()
+  const playerId = idPlayerDeleteInput.value
+  fetch(`${databaseURL}/players/${playerId}`,{
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json',
+    'Authorization': `Bearer ${getAccessToken()}`},
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  idPlayerDeleteInput.value = ''
+}
+
+  // Delete a tournament in the databse by entering the tournament ID
+
+  function deleteTournament(e) {
+    e.preventDefault()
+    const tournamentId = idTournamentDeleteInput.value
+    fetch(`${databaseURL}/tournaments/${tournamentId}`,{
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json',
+      'Authorization': `Bearer ${getAccessToken()}`},
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+    idTournamentDeleteInput.value = ''
+  }
--- user/user.js
+++ user/user.js
@@ -1,23 +1,15 @@
-// API URL cache
-
-const databaseURL = 'http://localhost:3005'
-
 // Cache html elements
 
+const playerNameInput = document.getElementById('player-name-input')
+const submitPlayerSearchButton = document.getElementById('submit-player-search-button')
+const searchedPlayerContainer = document.getElementById('searched-player-container')
 const showPlayersButton = document.getElementById('get-all-players-button')
-const playersDiv = document.getElementById('players-container')
-const loginButton = document.getElementById('login-button')
-const signupButton = document.getElementById('signup-button')
-const logoutButton = document.getElementById('logout-button')
-const adminViewButton = document.getElementById('admin-view-button')
+const playersDiv1 = document.getElementById('players-container-1')
 
 // Set event listeners
 
+submitPlayerSearchButton.addEventListener('click', findPlayer)
 showPlayersButton.addEventListener('click', getAllPlayers)
-adminViewButton.addEventListener('click', accessAdminPortal)
-loginButton.addEventListener('click', () => window.location.href = './login/login.html')
-signupButton.addEventListener('click', () => window.location.href = './signup/signup.html')
-logoutButton.addEventListener('click', logout)
 
 // Retrieve JWT token from local storage
 
@@ -25,37 +17,37 @@ function getAccessToken() {
   return localStorage.getItem('jwtToken')
 }
 
-// Check if a user is logged in
+// Functions
 
-document.addEventListener('DOMContentLoaded', () => {
-  const accessToken = getAccessToken()
-  if (!accessToken) {
-    loginButton.style.display = 'block'
-    signupButton.style.display = 'block'
-  } else {
-    fetch(`${databaseURL}/auth/verifyLoggedIn`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json',
-      'Authorization': `Bearer ${getAccessToken()}`},
-      body: '' })
-      .then(res => res.json())
-      .then(data => {
-        if (data !== true) {
-          loginButton.style.display = 'block'
-          signupButton.style.display = 'block'
-        } else {
-          logoutButton.style.display = 'block'
+// Get a player by name from API
+
+function findPlayer(e) {
+  e.preventDefault()
+  searchedPlayerContainer.innerHTML = ''
+  let playerNameValue = playerNameInput.value
+  playerNameValue = playerNameValue.replace(/\s/g,'_') // Replace spaces with underscores in name
+  fetch(`${databaseURL}/players/name/${playerNameValue}`)
+    .then(res => res.json())
+    .then(player => {
+      const newPlayerUl = document.createElement('ul')
+      newPlayerUl.classList.add('searched-player-ul')
+      Object.keys(player).forEach(key => {
+        const fields = ['fullName', 'age', 'nationality', 'worldRanking', 'lifetimeEarnings']
+        if (fields.includes(key)) {
+          const newPlayerFieldLi = document.createElement('li')
+          newPlayerFieldLi.classList.add('player-field-li')
+          newPlayerFieldLi.textContent = `${key}: ${player[key]}`
+          newPlayerUl.appendChild(newPlayerFieldLi)
         }
       })
-  }
-})
-
-// Functions
+      searchedPlayerContainer.appendChild(newPlayerUl)
+    })
+}
 
 // Get all Players from API
 
 function getAllPlayers() {
-  playersDiv.innerHTML = '';
+  playersDiv1.innerHTML = ''
   fetch(`${databaseURL}/players`, {
     method: 'GET',
     headers:  {'Authorization': `Bearer ${getAccessToken()}`}
@@ -64,27 +56,20 @@ function getAllPlayers() {
     .then(data => {
       if (!data.length > 0) return
       data.forEach(player => {
+        const columnCount = 3
         const newPlayerUl = document.createElement('ul')
         newPlayerUl.classList.add('player-ul')
         Object.keys(player).forEach(key => {
-          const newPlayerFieldLi = document.createElement('li')
-          newPlayerFieldLi.classList.add('player-field-li')
-          if (key === 'tournamentsPlayed') {
-            player[key].join(', ')
+          const fields = ['fullName', 'age', 'nationality', 'worldRanking', 'lifetimeEarnings']
+          if (fields.includes(key)) {
+            const newPlayerFieldLi = document.createElement('li')
+            newPlayerFieldLi.classList.add('player-field-li')
+            newPlayerFieldLi.textContent = `${key}: ${player[key]}`
+            newPlayerUl.appendChild(newPlayerFieldLi)
           }
-          newPlayerFieldLi.textContent = `${key}: ${player[key]}`
-          newPlayerUl.appendChild(newPlayerFieldLi)
         })
-        playersDiv.appendChild(newPlayerUl)
+        playersDiv1.appendChild(newPlayerUl)
       })
     })
 }
 
-function accessAdminPortal() {
-  window.location.href='./admin/admin.html'
-}
-
-function logout() {
-  localStorage.removeItem('jwtToken')
-  window.location.reload()
-}
