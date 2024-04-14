77274e42dc8782b937cdfa33a3afd0e1a873499d
Josh Logan <joshlogan103@gmail.com>
2024-04-14 19:22:05 -0400

    Update API URL

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,7 +1,6 @@
 // API URL cache
 
-//const APIURL = 'http://localhost:3005'
-const databaseURL = 'https://maximum-break-api-943feb008688.herokuapp.com'
+const APIURL = 'https://maximum-break-api-943feb008688.herokuapp.com/'
 
 // Cache html elements
 
